688bcf3a292178512d4ae361fa84883a6213b6b2
josephmathew900 <josephmathew900@gmail.com>
2021-03-06 15:00:28 +0530

    LoginForm

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@
 //import YoutubeForm from "./react_formik_demo/YoutubeForm";
 
 import "./formikcontrol.css";
-import FormikContainer from "./formik_controls_demo/FormikContainer";
+import LoginForm from "./formik_controls_demo/LoginForm";
+//import FormikContainer from "./formik_controls_demo/FormikContainer";
 
 function App() {
   return (
     <div className="App">
-      <FormikContainer />
+      <LoginForm />
     </div>
   );
 }
