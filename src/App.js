74497b3a0fe851b5ad7a5c600720aaea96a73367
josephmathew900 <josephmathew900@gmail.com>
2021-03-06 16:03:20 +0530

    EnrollmentForm

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 //import "./reactformik.css";
 //import YoutubeForm from "./react_formik_demo/YoutubeForm";
+//import LoginForm from "./formik_controls_demo/LoginForm";
+//import RegistrationForm from "./formik_controls_demo/RegistrationForm";
+//import FormikContainer from "./formik_controls_demo/FormikContainer";
 
 import "./formikcontrol.css";
-//import LoginForm from "./formik_controls_demo/LoginForm";
-import RegistrationForm from "./formik_controls_demo/RegistrationForm";
-//import FormikContainer from "./formik_controls_demo/FormikContainer";
+import EnrollmentForm from "./formik_controls_demo/EnrollmentForm";
 
 function App() {
   return (
     <div className="App">
-      <RegistrationForm />
+      <EnrollmentForm />
     </div>
   );
 }
