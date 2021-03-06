eb3dbef227c107fe88509d1f533c6094574d171c
josephmathew900 <josephmathew900@gmail.com>
2021-03-06 11:41:26 +0530

    RadioButton component

--- src/formik_controls_demo/FormikContainer.js
+++ src/formik_controls_demo/FormikContainer.js
@@ -4,22 +4,33 @@ import * as Yup from "yup";
 import FormikControl from "./FormikControl";
 
 function FormikContainer() {
-  const dropdownOption = [
+  const dropdownOptions = [
     { key: "Select an option", value: "" },
     { key: "Option 1", value: "option1" },
     { key: "Option 2", value: "option2" },
     { key: "Option 3", value: "option3" },
   ];
+
+  const radioOptions = [
+    { key: "Option 1", value: "rOption1" },
+    { key: "Option 2", value: "rOption2" },
+    { key: "Option 3", value: "rOption3" },
+  ];
+
   const initialValues = {
     email: "",
     description: "",
     selectOption: "",
+    radioOption: "",
   };
+
   const validationSchema = Yup.object({
     email: Yup.string().required("Required!"),
     description: Yup.string().required("Required!"),
     selectOption: Yup.string().required("Required!"),
+    radioOption: Yup.string().required("Required!"),
   });
+
   const onSubmit = (values) => {
     console.log("Form data", values);
   };
@@ -49,8 +60,16 @@ function FormikContainer() {
             control="select"
             label="Select a topic"
             name="selectOption"
-            options={dropdownOption}
+            options={dropdownOptions}
           />
+
+          <FormikControl
+            control="radio"
+            label="Radio Topic"
+            name="radioOption"
+            options={radioOptions}
+          />
+
           <button type="submit">Submit</button>
         </Form>
       )}
